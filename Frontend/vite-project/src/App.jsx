5a076636a53df477b02f667a6a212e3dd563d495
sanyam <sanyammadaanfca@gmail.com>
2024-11-16 15:16:02 +0530

    Frontend completed

--- Frontend/vite-project/src/App.jsx
+++ Frontend/vite-project/src/App.jsx
@@ -1,15 +1,18 @@
 import { useState } from "react"
 import SignUp from "./Components/SignUp"
 import Login from './Components/Login'
+import Dashboard from './Components/Dashboard'
+import AddCar from "./Components/AddCar"
 import {BrowserRouter,Routes,Route} from 'react-router-dom';
 
 function App() {
-  const [count, setCount] = useState(0)
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<SignUp/>}></Route>
         <Route path="/login" element={<Login/>}></Route>
+        <Route path="/dashboard" element={<Dashboard/>}></Route>
+        <Route path="/addcar" element={<AddCar/>}></Route>
       </Routes>
     </BrowserRouter>
   )

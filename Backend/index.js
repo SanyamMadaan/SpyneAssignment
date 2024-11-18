3211ba0da021437260bafda09ee619ff1f299d72
sanyam <sanyammadaanfca@gmail.com>
2024-11-18 11:13:09 +0530

    solved swagger issue

--- Backend/index.js
+++ Backend/index.js
@@ -1,26 +1,34 @@
-const express=require('express');
-const cors=require('cors');
+const swaggerUi = require('swagger-ui-express');
+const express = require('express');
+const cors = require('cors');
 require('dotenv').config();
-const PORT=process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 require('./db/index');
-const UserRouter=require('./routes/UserRouter');
-const ProductRouter=require('./routes/ProductRouter');
-const swaggerUi=require('swagger-ui-express');
-const swaggerSpec=require('./swaggerConfig');
+const UserRouter = require('./routes/UserRouter');
+const ProductRouter = require('./routes/ProductRouter');
+const path = require('path');
 
-const app=express();
+const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use('/api/v1/user',UserRouter);
-app.use('/api/v1/products',ProductRouter);
+app.use('/api/v1/user', UserRouter);
+app.use('/api/v1/products', ProductRouter);
 
-const swaggerOptions={
-    swaggerUrl:'/api/v1/docs/swagger.json'
-}
-//set up Swagger UI
-app.use('/api/v1/docs',swaggerUi.serve,swaggerUi.setup(swaggerSpec,swaggerOptions));
+// Serve Swagger UI
+app.use(
+    '/api/v1/docs',
+    swaggerUi.serve,
+    swaggerUi.setup(null, {
+      swaggerOptions: { url: '/swagger.json' }, // URL of the swagger.json file
+    })
+  );
+  
 
-app.listen(PORT,()=>{
-    console.log(`App is listening at http://localhost:${PORT}`);
-})
+app.get('/swagger.json', (req, res) => {
+    res.sendFile(path.join(__dirname, 'swagger.json'));
+  });
+
+app.listen(PORT, () => {
+  console.log(`App is listening at http://localhost:${PORT}`);
+});
